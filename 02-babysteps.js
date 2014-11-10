e8a8e3bface364049daa0a0f1b0bf7830beb0ab4
Fadie Hannona <fadiehannona@gmail.com>
2014-11-10 19:29:08 +0000

    Hello World complete

---
+++ 02-babysteps.js
@@ -0,0 +1,12 @@
+// [node, 01-babysteps, 45, 56, 67]
+//    0       1         2   3    4
+
+// var sum = 0
+//
+// for (var i = 2; i < process.argv.length; i++) {
+//   sum = sum + process.argv[i]
+// }
+//
+// console.log(sum)
+
+console.log(process.argv)
